3a04e204cbc1eebbc89024eaf0c082e7e318f6e1
Koki Tanaka <67k.tanaka@gmail.com>
2026-07-06 23:37:41 +0900

    fix: make package installable and include sensor launch in raw recording

--- humble_ws/src/fastlio_go2w_bringup/setup.py
+++ humble_ws/src/fastlio_go2w_bringup/setup.py
@@ -7,7 +7,8 @@ package_name = "fastlio_go2w_bringup"
 setup(
     name=package_name,
     version="0.1.0",
-    packages=find_packages(exclude=("test",)),
+    packages=find_packages("src", exclude=("test",)),
+    package_dir={"": "src"},
     data_files=[
         ("share/ament_index/resource_index/packages", [f"resource/{package_name}"]),
         (f"share/{package_name}", ["package.xml"]),
